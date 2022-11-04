07fc41667b255c835d51d9603f7b2557a231c89c
Peter Morganelli <97714941+petermorganelli@users.noreply.github.com>
2022-11-03 20:48:55 -0400

    make it the best

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const e = require("express");
 if(process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.get("*", (req,res)=> {
-    res.sendFile(path.resolve(__dirname, "build", "index.html" ))
+    res.sendFile(path.resolve(__dirname, "/build", "index.html" ))
   });
 }
 
