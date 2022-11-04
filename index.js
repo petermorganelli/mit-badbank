c24b359945976c33db4a96fd73f858e32fad97a2
Peter Morganelli <97714941+petermorganelli@users.noreply.github.com>
2022-11-03 21:14:38 -0400

    fixed bug

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ const e = require("express");
 
 
 // used to serve static files from public directory
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req,res)=> {
-    res.sendFile(path.resolve(__dirname, "/build", "index.html" ))
-  });
-}
+app.use(express.static("build"));
 
 app.use(cors());
 
